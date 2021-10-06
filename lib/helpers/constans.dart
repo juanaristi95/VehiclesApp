fb668a4a5bf1ba3aad9f0cea4590078ebfc2b915
juanaristi95 <46731700+juanaristi95@users.noreply.github.com>
2021-10-05 22:16:22 -0500

    Show menu

---
+++ lib/helpers/constans.dart
@@ -0,0 +1,3 @@
+class Constans {
+  static String get apiUrl => 'https://vehiculesjdaitm2021.azurewebsites.net';
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
+import 'package:http/http.dart' as http;
+import 'package:vehicles_app/components/loader_component.dart';
+
+import 'package:vehicles_app/helpers/constans.dart';
+import 'package:vehicles_app/models/token.dart';
+import 'package:vehicles_app/screens/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({ Key? key }) : super(key: key);
@@ -8,12 +16,236 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String _email = 'luis@yopmail.com';
+  String _emailError = '';
+  bool _emailShowError = false;
+
+  String _password = '123456';
+  String _passwordError = '';
+  bool _passwordShowError = false;
+
+  bool _rememberme = true;
+  bool _passwordShow = false;
+
+  bool _showLoader = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login screen')
+      body: Stack(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _showLogo(),
+              SizedBox(height: 20,),  
+              _showEmail(),
+              _showPassword(),
+              _showRememberme(),
+              _showButtons(),
+            ],
+          ),
+          _showLoader ? LoaderComponent(text: 'Por favor espere...',) : Container(),
+        ],
       ),
     );
+  }
+
+  Widget _showLogo() {
+    return Image(
+      image: AssetImage('assets/vehicles_logo.png'),
+      width: 200,
+    );
+  }
+
+  Widget _showEmail() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        autofocus: true,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'Ingresa tú email...',
+          labelText: 'Email',
+          errorText: _emailShowError ? _emailError : null,
+          prefixIcon: Icon(Icons.alternate_email),
+          suffixIcon: Icon(Icons.email),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _email = value;
+          //print(_email);
+        },
+      ),
+    );
+  }
+
+  Widget _showPassword() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextField(
+        obscureText: !_passwordShow,
+        decoration: InputDecoration(
+          hintText: 'Ingresa tú Contraseña...',
+          labelText: 'Contraseña',
+          errorText: _passwordShowError ? _passwordError : null,
+          prefixIcon: Icon(Icons.lock),
+          suffixIcon: IconButton(
+            icon: _passwordShow  ? Icon(Icons.visibility) : Icon(Icons.visibility_off),
+            onPressed: (){
+              setState(() {
+                _passwordShow = !_passwordShow;  
+              });
+            },
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+        onChanged: (value) {
+          _password = value;
+        },
+      ),
+    );
+  }
+
+  Widget _showRememberme() {
+    return CheckboxListTile(
+      title: Text('Recordarme'),
+      value: _rememberme,
+      onChanged: (value) {  
+        setState(() {
+          _rememberme = value!;  
+        });
+      },
+    );  
+  }
+
+  Widget _showButtons() {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Expanded(
+            child: ElevatedButton(
+              child: Text('Iniciar Sesion'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states){
+                    return Color(0xFF120E43);
+                  }
+                ),
+              ),
+              onPressed: () => _login(), 
+            ),
+          ),
+          SizedBox(width: 20,),
+          Expanded(
+            child: ElevatedButton(
+              child: Text('Nuevo Usuario'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states){
+                    return Color(0xFF6A1B4D);
+                  }
+                ),
+              ),              
+              onPressed: (){}, 
+            ),
+          ),        
+        ],
+      ),
+    );
+  }
+
+  void _login() async {
+    setState(() {
+      _passwordShow = false;
+    });
+    if(!_ValidateFields()){
+      return;
+    }
+
+    setState(() {
+      _showLoader = true;
+    });
+
+    Map<String, dynamic> request = {
+      'userName': _email,
+      'password': _password
+    };
+
+    var url = Uri.parse('${Constans.apiUrl}/api/Account/CreateToken');
+    var response = await http.post(
+      url,
+      headers: {
+        'content-type' : 'application/json',
+        'accept' : 'application/json',
+      },
+      body: jsonEncode(request),
+    );  
+
+    setState(() {
+      _showLoader = false;
+    });
+
+    if(response.statusCode >= 400){
+      setState(() {
+        _passwordShowError =  true;
+        _passwordError = 'Email o contraseña incorrecto';
+      });
+      return;
+    }
+
+    //print(response.body);
+    var body = response.body;
+    var decodeJson = jsonDecode(body);
+    var token = Token.fromJson(decodeJson);
+    //print(token.token);
+    Navigator.pushReplacement(
+      context, 
+      MaterialPageRoute(
+        builder: (context) => HomeScreen(token: token,)
+      )
+    );
+  }
+
+  bool _ValidateFields() {
+    bool isValid = true;
+    if (_email.isEmpty) {
+      isValid = false;
+      _emailShowError = true;
+      _emailError = 'Debes ingresar tú email';
+
+    } else if (!EmailValidator.validate(_email)) {
+      isValid = false;
+      _emailShowError = true;
+      _emailError = 'Debes ingresar un email valido';
+    }
+    else {
+      _emailShowError = false;
+    }
+
+    if (_password.isEmpty) {
+      isValid = false;
+      _passwordShowError = true;
+      _passwordError = 'Debes ingresar tú Contraseña';
+
+    } else if (_password.length < 6) {
+      isValid = false;
+      _passwordShowError = true;
+      _passwordError = 'Debes ingresar una Contraseña de al menos 6 carácteres';
+    }
+    else {
+      _passwordShowError = false;
+    }
+
+    setState(() {
+
+    });
+    return isValid;
   }
 }
